a33e13a580f61b159a3383024c871f4478bcaff2
Mathias Ravn-Nielsen <mathias.ravnnielsen@yahoo.dk>
2022-11-10 11:36:44 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php // Opening PHP tag - nothing should be before this, not even whitespace
+// Custom Function to Include
+function my_favicon_link() {
+	echo '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />' . "\n";
+}
+add_action( 'wp_head', 'my_favicon_link' );
